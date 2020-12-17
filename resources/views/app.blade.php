d4ed3fce74e04bdf014604c13432e2986d10bebb
MikeHanon <mike.hanon@gmail.com>
2020-12-17 09:22:30 +0100

    do not display footer about us link on about page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,7 +15,11 @@
         @yield('content')
         <footer>
             <p>&copy; Copyright {{date('Y')}} 
+
+                @if (! Route::is('about'))
                 &middot; <a href="{{route('about')}}">About Us</a>
+                @endif
+               
             </p>
         </footer>
     </body>
